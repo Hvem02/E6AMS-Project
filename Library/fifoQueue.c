0eb1b815215034cc72f9b198b59d88ed9a6b8721
SørenHN <sorenherskindnielsen@gmail.com>
2018-05-17 13:44:35 +0200

    Adds fifoQueue library.

---
+++ Library/fifoQueue.c
@@ -0,0 +1,127 @@
+#include "fifoQueue.h"
+
+//***************************************************************
+// Macro Functions                                              *
+//***************************************************************
+#define RETURN_ON_ERROR(EXPRESSION) \
+do                                  \
+{                                   \
+    uint8_t retVal = (EXPRESSION);  \
+    if(retVal != FIFO_SUCCES)       \
+    {                               \
+        return retVal;              \
+    }                               \
+}while(0)
+
+//***************************************************************
+// Static Function Declaration                                  *
+//***************************************************************
+static uint8_t isFull(FifoQueue const * queue);
+static uint8_t isEmpty(FifoQueue const * queue);
+
+static uint16_t head(FifoQueue const * queue);
+static uint16_t tail(FifoQueue const * queue);
+
+//***************************************************************
+// Public Function Implementation                               *
+//***************************************************************
+FifoQueue fifoInit(uint8_t* const buffer, uint16_t size)
+{
+    FifoQueue fifo =
+    {
+        .buffer = buffer,
+        .bufferSize = size,
+        .nextIndex = 0,
+        .usedSize = 0,
+    };
+    return fifo;
+}
+
+uint8_t push(FifoQueue* queue, uint8_t value)
+{
+    RETURN_ON_ERROR(isFull(queue));
+    queue->buffer[queue->nextIndex] = value;
+    queue->nextIndex = (queue->nextIndex + 1) % queue->bufferSize;
+    queue->usedSize++;
+    return FIFO_SUCCES;
+}
+
+uint8_t pop(FifoQueue* queue, uint8_t* retValue)
+{
+    RETURN_ON_ERROR(isEmpty(queue));
+    *retValue = queue->buffer[tail(queue)];
+    queue->usedSize--;
+    return FIFO_SUCCES;
+}
+
+uint8_t clear(FifoQueue* queue)
+{
+    queue->nextIndex = 0;
+    queue->usedSize = 0;
+    return FIFO_SUCCES;
+}
+
+uint8_t fill(FifoQueue* queue, uint8_t fillValue)
+{
+    RETURN_ON_ERROR(clear(queue));
+    for(uint16_t i = 0; i < queue->bufferSize; i++)
+    {
+        RETURN_ON_ERROR(push(queue, fillValue));
+    }
+    return FIFO_SUCCES;
+}
+
+uint8_t peak(FifoQueue const * queue, uint8_t* retValue)
+{
+    return peakAt(queue, retValue, 0);
+}
+
+uint8_t peakAt(FifoQueue const * queue, uint8_t* retValue, uint16_t peakIndex)
+{
+    RETURN_ON_ERROR(isEmpty(queue));
+    if(peakIndex >= queue->usedSize)
+    {
+        return FIFO_ERROR_OUT_OF_BOUNDS;
+    }
+
+    uint16_t bufferIndex = (tail(queue) + peakIndex) % queue->bufferSize;
+    *retValue = queue->buffer[bufferIndex];
+    return FIFO_SUCCES;
+}
+
+//***************************************************************
+// Static Function Implementation                               *
+//***************************************************************
+static uint8_t isFull(FifoQueue const * queue)
+{
+    if(queue->usedSize == queue->bufferSize)
+    {
+        return FIFO_ERROR_FULL;
+    }
+    else
+    {
+        return FIFO_SUCCES;
+    }
+}
+
+static uint8_t isEmpty(FifoQueue const * queue)
+{
+    if(queue->usedSize == 0)
+    {
+        return FIFO_ERROR_EMPTY;
+    }
+    else
+    {
+        return FIFO_SUCCES;
+    }
+}
+
+static uint16_t head(FifoQueue const * queue)
+{
+    return (queue->nextIndex + queue->usedSize) % queue->bufferSize;
+}
+
+static uint16_t tail(FifoQueue const * queue)
+{
+    return ((queue->bufferSize - queue->usedSize) + queue->nextIndex) % queue->bufferSize;
+}
---
+++ Library/fifoQueue.h
@@ -0,0 +1,81 @@
+#ifndef FIFOQUEUE_H
+#define FIFOQUEUE_H
+
+#include <stdint-gcc.h>
+
+//***************************************************************
+// Return values                                                *
+//***************************************************************
+#define FIFO_SUCCES                 0x00
+#define FIFO_ERROR_EMPTY            0x01
+#define FIFO_ERROR_FULL             0x02
+#define FIFO_ERROR_OUT_OF_BOUNDS    0x03
+
+/**
+ *
+ */
+typedef struct FifoQueue
+{
+    uint8_t* const buffer;      /** Pointer to buffer. */
+    uint16_t const bufferSize;  /** Size of the buffer. */
+    uint16_t nextIndex;         /** Index of next available buffer index */
+    uint16_t usedSize;          /** Size of the used part of buffer. */
+} FifoQueue;
+
+/**
+ *
+ * @param buffer    Pointer to buffer going to be used as queue.
+ * @param size      The size of the buffer.
+ * @return          Returns 0 on succes.
+ */
+FifoQueue fifoInit(uint8_t* const buffer, uint16_t size);
+
+/**
+ *
+ * @param queue     Pointer to queue.
+ * @param value     Value to be pushed onto queue.
+ * @return          Returns 0 on succes.
+ */
+uint8_t push(FifoQueue* queue, uint8_t value);
+
+/**
+ *
+ * @param queue     Pointer to queue.
+ * @param retVal    Pointer to where value should be returned to.
+ * @return          Returns 0 on succes.
+ */
+uint8_t pop(FifoQueue* queue, uint8_t* retValue);
+
+/**
+ *
+ * @param queue     Pointer to queue.
+ * @return          Returns 0 on succes.
+ */
+uint8_t clear(FifoQueue* queue);
+
+/**
+ *
+ * @param queue     Pointer to queue.
+ * @param fillValue Value to be used to fill the queue.
+ * @return          Returns 0 on succes.
+ */
+uint8_t fill(FifoQueue* queue, uint8_t fillValue);
+
+/**
+ *
+ * @param queue     Pointer to queue.
+ * @param retVal    Pointer to where value should be returned to.
+ * @return          Returns 0 on succes.
+ */
+uint8_t peak(FifoQueue const * queue, uint8_t* retValue);
+
+/**
+ *
+ * @param queue     Pointer to queue.
+ * @param retValue  Pointer to where value should be returned to.
+ * @param peakIndex Index of the value wished to be peaked.
+ * @return          Returns 0 on succes.
+ */
+uint8_t peakAt(FifoQueue const * queue, uint8_t* retValue, uint16_t peakIndex);
+
+#endif //FIFOQUEUE_H
