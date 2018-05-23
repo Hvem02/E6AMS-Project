16bd6ac96778e720523ea1fbbf8105b93c92827a
SørenHN <sorenherskindnielsen@gmail.com>
2018-05-23 14:36:07 +0200

    Adds instructions for removing button event-handling-funtions, in the documentation.

--- drivers/buttonInterface.h
+++ drivers/buttonInterface.h
@@ -16,48 +16,56 @@ void buttonInterfaceInit(void);
 
 /**
  * Function for adding handler function for the UP button being pushed.
+ * To remove the handler function, call this function with a NULL pointer.
  * @param callbackFunc
  */
 void buttonSetUpPushCallback(buttonCallback_t callbackFunc);
 
 /**
  * Function for adding handler function for the UP button being released.
+ * To remove the handler function, call this function with a NULL pointer.
  * @param callbackFunc  Function pointer to handler function.
  */
 void buttonSetUpReleaseCallback(buttonCallback_t callbackFunc);
 
 /**
  * Function for adding handler function for the DOWN button being pushed.
+ * To remove the handler function, call this function with a NULL pointer.
  * @param callbackFunc  Function pointer to handler function.
  */
 void buttonSetDownPushCallback(buttonCallback_t callbackFunc);
 
 /**
  * Function for adding handler function for the DOWN button being released.
+ * To remove the handler function, call this function with a NULL pointer.
  * @param callbackFunc  Function pointer to handler function.
  */
 void buttonSetDownReleaseCallback(buttonCallback_t callbackFunc);
 
 /**
  * Function for adding handler function for the LEFT button being pushed.
+ * To remove the handler function, call this function with a NULL pointer.
  * @param callbackFunc  Function pointer to handler function.
  */
 void buttonSetLeftPushCallback(buttonCallback_t callbackFunc);
 
 /**
  * Function for adding handler function for the LEFT button being released.
+ * To remove the handler function, call this function with a NULL pointer.
  * @param callbackFunc  Function pointer to handler function.
  */
 void buttonSetLeftReleaseCallback(buttonCallback_t callbackFunc);
 
 /**
  * Function for adding handler function for the RIGHT button being pushed.
+ * To remove the handler function, call this function with a NULL pointer.
  * @param callbackFunc  Function pointer to handler function.
  */
 void buttonSetRightPushCallback(buttonCallback_t callbackFunc);
 
 /**
  * Function for adding handler function for the RIGHT button being released.
+ * To remove the handler function, call this function with a NULL pointer.
  * @param callbackFunc  Function pointer to handler function.
  */
 void buttonSetRightReleaseCallback(buttonCallback_t callbackFunc);
