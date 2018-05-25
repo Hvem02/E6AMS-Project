abc0688a12393305e7bf602fc07162b4f471587f
Munke <aalsing@gmail.com>
2018-05-25 11:46:03 +0200

    Update cmakelist to strip on release.

--- Config/transferElf.sh
+++ Config/transferElf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 work_dir=`pwd`
-avrdude -C avrdude.conf -P/dev/ttyACM0 -cwiring -patmega2560 -b115200 -v -D -Uflash:w:../cmake-build-debug/E6AMS
+avrdude -C avrdude.conf -P/dev/ttyACM0 -cwiring -patmega2560 -b115200 -v -D -Uflash:w:../cmake-build-release/E6AMS
